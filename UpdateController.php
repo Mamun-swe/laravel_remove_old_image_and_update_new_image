c8000045213bf5e159d3df7c3afaecf3e1e4e502
Abdullah Al Mamun <46353651+Mamun-swe@users.noreply.github.com>
2020-10-27 17:53:50 +0600

    Create UpdateController.php

---
+++ UpdateController.php
@@ -0,0 +1,64 @@
+    public function update(Request $request, $id)
+    {
+        $valid = false;
+        $rules = [
+            'name' => 'required',
+            'cashback' => 'required',
+            'start_date' => 'required',
+            'end_date' => 'required',
+            'short_description' => 'required'
+        ];
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => $validator->messages(),
+            ], 422);
+        }else{
+            if ($request->hasFile('image')){
+                $file = $request->file('image');
+                $extension = $file->getClientOriginalExtension();
+                $filename = time(). '.' . $extension;
+                
+
+                $data = Campaign::find($id);
+                $old_image = public_path().'/campaigns/'.$data->image;
+                unlink($old_image);
+
+                $file->move('campaigns', $filename);
+                $form_data = array(
+                    'name' => $request->name,
+                    'cashback' => $request->cashback,
+                    'start_date' => $request->start_date,
+                    'end_date' => $request->end_date,
+                    'short_description' => $request->short_description,
+                    'image' => $filename
+                );
+
+                Campaign::where('id', '=', $id)->update($form_data);
+                $valid = true;
+
+                if($valid == true) {
+                    return response()->json(['message' => 'Successfully updated' ], 200);
+                } else {
+                    return response()->json(['message' => 'Failed to update'], 204);
+                }
+            }else{
+                $form_data = array(
+                    'name' => $request->name,
+                    'cashback' => $request->cashback,
+                    'start_date' => $request->start_date,
+                    'end_date' => $request->end_date,
+                    'short_description' => $request->short_description
+                );
+
+                Campaign::where('id', '=', $id)->update($form_data);
+                $valid = true;
+
+                if($valid == true) {
+                    return response()->json(['message' => 'Successfully updated' ], 200);
+                } else {
+                    return response()->json(['message' => 'Failed to update'], 204);
+                }
+            }
+        }
+    }
